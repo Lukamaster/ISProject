f0378dc6d576831c9e04b5f8980f183a61c85589
Luka Cheshlarov <luka.ceslarov@live.com>
2024-07-01 00:44:40 +0200

    Added Repository layer for all objects of type BaseEntity

--- ISProject.Domain/MusicRecord.cs
+++ ISProject.Domain/MusicRecord.cs
@@ -11,9 +11,11 @@ namespace ISProject.Domain
         public string Title { get; set; }
         public string Description { get; set; }
         public string Artist { get; set; }
-        public double price { get; set; }
-        public int volume { get; set; }
+        public double Price { get; set; }
+        public int Volume { get; set; }
         public bool InStock { get; set; }
         public string ImageURL { get; set; }
+        public ICollection<MusicRecordInOrder>? MusicRecordInOrders { get; set; }
+        public virtual ICollection<MusicRecordInShoppingCart>? MusicRecordsInShoppingCart { get; set; }
     }
 }
---
+++ ISProject.Repository/Interface/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using ISProject.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISProject.Repository.Interface
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        IEnumerable<T> GetAll();
+        T Get(Guid? id);
+        T Insert(T entity);
+        T Update(T entity);
+        T Delete(T entity);
+    }
+}
